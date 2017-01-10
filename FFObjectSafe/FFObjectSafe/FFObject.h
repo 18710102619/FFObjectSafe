38f5049788f9499469bc927b7c675c982d41eebd
张玲玉 <2949820345@qq.com>
2017-01-10 14:37:41 +0800

    NSObject

---
+++ FFObjectSafe/FFObjectSafe/FFObject.h
@@ -0,0 +1,13 @@
+//
+//  FFObject.h
+//  FFObjectSafe
+//
+//  Created by 张玲玉 on 2017/1/10.
+//  Copyright © 2017年 bj.zly.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFObject : NSObject
+
+@end
---
+++ FFObjectSafe/FFObjectSafe/FFObject.m
@@ -0,0 +1,13 @@
+//
+//  FFObject.m
+//  FFObjectSafe
+//
+//  Created by 张玲玉 on 2017/1/10.
+//  Copyright © 2017年 bj.zly.com. All rights reserved.
+//
+
+#import "FFObject.h"
+
+@implementation FFObject
+
+@end
